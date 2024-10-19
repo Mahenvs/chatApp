5cc34831d8f48c657d420a76f870d279592330f0
mahenv <mahendrauppari044@gmail.com>
2024-10-19 17:16:27 -0400

    moved url to env

--- app/lib/url.ts
+++ app/lib/url.ts
@@ -1,4 +1,4 @@
-export const Url = `${process.env.NEXT_PUBLIC_API}api/`
+export const Url = `${process.env.NEXT_PUBLIC_API}`+"/api/"
 export const searchUserUrl = Url+"user/searchuser"
 export const getUsersUrl = Url +"user" 
 export const saveChatUrl = Url +"user/chat" 
