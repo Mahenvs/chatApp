22233de2ff3ef14496f888be30a3b724d11a3916
mahenv <mahendrauppari044@gmail.com>
2024-10-19 03:32:52 -0400

    updated prod url

--- app/lib/url.ts
+++ app/lib/url.ts
@@ -1,4 +1,4 @@
-export const Url = "http://localhost:3000/api/"
+export const Url = "https://chat-py9ch7t7l-mahenvs-projects.vercel.app/api/"
 export const searchUserUrl = Url+"user/searchuser"
 export const getUsersUrl = Url +"user" 
 export const saveChatUrl = Url +"user/chat" 
